bf8b4f2c07995a641bac29f972e4c744094ad02d
abhi_13_7 <abhichougale13.7@gmail.com>
2024-08-16 18:47:20 +0530

    cosmatic changes 3

--- src/components/employment-details/employment-details.component.ts
+++ src/components/employment-details/employment-details.component.ts
@@ -45,6 +45,6 @@ export class EmploymentDetailsComponent {
       '_blank',
       'toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400'
     );
-    return null;
+    return false;
   }
 }
